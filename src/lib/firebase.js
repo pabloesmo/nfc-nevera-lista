cf3eb1a8032041f0ef97a8030566abd830f72d6e
pabloesmo <pabloesmo@hotmail.com>
2026-09-17 23:06:28 +0200

    add de configuracion firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCrv71BVdh7ZPbTRcVkuuuFXMREcmlJGu0",
   authDomain: "nfc-fridge-compra.firebaseapp.com",
@@ -11,7 +9,8 @@ const firebaseConfig = {
   storageBucket: "nfc-fridge-compra.firebasestorage.app",
   messagingSenderId: "134122647316",
   appId: "1:134122647316:web:5c91e00f0c2a6d9e855c01"
-};
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const auth = getAuth(app)
